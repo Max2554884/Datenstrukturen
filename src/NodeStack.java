b94fed1d76dacd6a974e74992b02cc0f80b20f20
max2554884 <max.wurmbach@t-online.de>
2025-01-31 10:25:06 +0100

    fein schlif 2

---
+++ src/NodeStack.java
@@ -0,0 +1,23 @@
+public class NodeStack <E> {
+
+    public NodeStack(E pContent) {
+
+    }
+
+    public E getContent() {
+        return null;
+    }
+
+    public void setContent(E pContent) {
+
+    }
+
+    public NodeStack getNextNode() {
+        return null;
+    }
+
+    public void  setNextNode(NodeStack nextNode) {
+
+
+    }
+}
---
+++ src/Rechnung.java
@@ -0,0 +1,22 @@
+public class Rechnung {
+
+ private int rechnungsnr, betrag;
+
+private String datum;
+
+public Rechnung(String pDatum, int pRechnungsnr, int pBetrag){
+
+ }
+
+ public int getrechnungsnr(){
+    return rechnungsnr;
+ }
+
+ public int  getbetrag(){
+  return betrag;
+ }
+
+ public String  getDatum(){
+    return datum;
+ }
+}
