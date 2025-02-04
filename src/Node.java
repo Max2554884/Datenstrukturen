d87186f14c920dbc8dee0e91e4316c89997510ee
max2554884 <max.wurmbach@t-online.de>
2025-02-04 13:00:32 +0100

    List

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,28 @@
-public class Node {
+
+public class Node<T>
+{
+    private T zeigeAuf;                             //Speichert das Objekt auf welches der Knoten zeigt
+    private Node nachfolger;                      //Speichert den nachfolgenden Knoten
+
+
+    public Node(T pZeigeAuf)
+    {
+        zeigeAuf = pZeigeAuf;
+        nachfolger = null;
+    }
+
+    public Node getNachfolger()
+    {
+        return nachfolger;
+    }
+
+    public void setNachfolger(Node pNachfolger)
+    {
+        this.nachfolger = pNachfolger;
+    }
+
+    public T getZeigeAuf()
+    {
+        return zeigeAuf;
+    }
 }
