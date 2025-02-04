ca63f32bdd77387c8b4d282301947ad02923728d
max2554884 <max.wurmbach@t-online.de>
2025-02-04 13:08:54 +0100

    List

--- src/List.java
+++ src/List.java
@@ -6,7 +6,20 @@ public class List {
 
     }
     public void append(ContentType pC){
+       Node hilfe = new Node(pInhalt);
+        if (erster == null) {
+            // Wenn die Warteschlange leer ist, wird der Kunde der erste
+            erster = hilfe;
+        } else {
+            // Sonst zum letzten Kunden führen und den neuen anhängen
+            Node aktueller = erster;
 
+            while (aktueller.getNachfolger() != null) {  // Schleife läuft so lange, wie es einen Nachfolger gibt
+                aktueller = aktueller.getNachfolger(); // Nachfolger wird zu aktueller
+            }
+            aktueller.setNachfolger(hilfe); // Mit aktueller.setNachfolger(pKunde) wird der neue Kunde (pKunde) als Nachfolger des letzten Kunden gesetzt.
+        }
+    }
     }
     public void next(){
 
