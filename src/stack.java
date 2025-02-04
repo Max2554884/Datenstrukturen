d12cc7f201e6f9a80c7b4883237c59d99aeb5dfb
max2554884 <max.wurmbach@t-online.de>
2025-02-04 12:30:59 +0100

    List

--- src/stack.java
+++ src/stack.java
@@ -4,9 +4,21 @@ public class stack <E>
 
       return null;
 }
- public void add (E content){
+    public void einfuegen(E pInhalt) {
+        Knoten hilfe = new Knoten(pInhalt);
+        if (erster == null) {
+            // Wenn die Warteschlange leer ist, wird der Kunde der erste
+            erster = hilfe;
+        } else {
+            // Sonst zum letzten Kunden führen und den neuen anhängen
+            Knoten aktueller = erster;
 
- }
+            while (aktueller.getNachfolger() != null) {  // Schleife läuft so lange, wie es einen Nachfolger gibt
+                aktueller = aktueller.getNachfolger(); // Nachfolger wird zu aktueller
+            }
+            aktueller.setNachfolger(hilfe); // Mit aktueller.setNachfolger(pKunde) wird der neue Kunde (pKunde) als Nachfolger des letzten Kunden gesetzt.
+        }
+    }
 
 public void remove(){
 
