29196523fb2fb0c5e4c850bd5005dd2f13740a26
max2554884 <max.wurmbach@t-online.de>
2025-02-04 12:48:19 +0100

    List

--- src/List.java
+++ src/List.java
@@ -1,2 +1,35 @@
+import jdk.jfr.ContentType;
+
 public class List {
+    public List()
+    {
+
+    }
+    public void append(ContentType pC){
+
+    }
+    public void next(){
+
+    }
+    public void insert(ContentType pC){
+
+    }
+    public void toFirst(){
+
+    }
+    public void toLast(){
+
+    }
+    public void remove(){
+
+    }
+      public boolean hasAccess (){
+
+    }
+      public ContentType getContent(){
+
+    }
+      public void setContent(ContentType pC){
+
+      }
 }
